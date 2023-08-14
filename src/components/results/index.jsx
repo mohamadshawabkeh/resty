59325d4218f50ba9037b6328da106b98f1c7866d
mohamadshawabkeh <mohamadshawabkeh45@outlook.com>
2023-08-14 14:45:33 +0300

    baseUpdate

---
+++ src/components/results/index.jsx
@@ -0,0 +1,10 @@
+
+function Results(props) {
+  return (
+    <section>
+      <pre>{props.data ? JSON.stringify(props.data, undefined, 2) : null}</pre>
+    </section>
+  );
+}
+
+export default Results;
